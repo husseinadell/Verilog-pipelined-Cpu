daa19995232befff562bd97836277d743a917791
husseinadell <31420582+husseinadell@users.noreply.github.com>
2017-12-03 19:56:40 +0200

    edited the AluControl and added PC again

--- ALUcontrol.v
+++ ALUcontrol.v
@@ -7,14 +7,14 @@ wire [6:0] ALUControlIn;
 assign test = {ALUOp,Function};
 always @(test)
 casex (test)
- 7'b11xxxxxx: begin ALUControl=3'b000; JRControl=1'b0; end //addi,lw,sw
- 7'b01xxxxxx: begin ALUControl=3'b001; JRControl=1'b0; end //beq
- 7'b00000000: begin ALUControl=3'b000; JRControl=1'b0; end //add
- 7'b00000001: begin ALUControl=3'b001; JRControl=1'b0; end //sub
- 7'b00000010: begin ALUControl=3'b010; JRControl=1'b0; end //and
- 7'b00000011: begin ALUControl=3'b011; JRControl=1'b0; end //or
- 7'b00000100: begin ALUControl=3'b100; JRControl=1'b0; end //slt
- 7'b00000100: begin ALUControl=3'b100; JRControl=1'b0; end //slti
+ 8'b11xxxxxx: begin ALUControl=3'b000; JRControl=1'b0; end //addi,lw,sw
+ 8'b01xxxxxx: begin ALUControl=3'b001; JRControl=1'b0; end //beq
+ 8'b00000000: begin ALUControl=3'b000; JRControl=1'b0; end //add
+ 8'b00000001: begin ALUControl=3'b001; JRControl=1'b0; end //sub
+ 8'b00000010: begin ALUControl=3'b010; JRControl=1'b0; end //and
+ 8'b00000011: begin ALUControl=3'b011; JRControl=1'b0; end //or
+ 8'b00000100: begin ALUControl=3'b100; JRControl=1'b0; end //slt
+ 8'b00000100: begin ALUControl=3'b100; JRControl=1'b0; end //slti
  8'b00001000: begin ALUControl=3'b000; JRControl=1'b1; end //jr
  default: 	  begin ALUControl=3'b000; JRControl=1'b0; end 
  endcase
